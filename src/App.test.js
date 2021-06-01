db15cb3d05a75707673a5e9b26e875f8c7e5bb7e
alexyli <alexyukewli@gmail.com>
2021-05-31 20:55:00 -0400

    feat: test exist

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Enzyme from "enzyme";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
+import App from "./App";
 
-import { render, screen } from '@testing-library/react';
-import App from './App';
+Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('render non-empty component without crashing', () => {
+  const wrapper = shallow(<App />)
+  expect(wrapper.exists()).toBe(true);
 });
